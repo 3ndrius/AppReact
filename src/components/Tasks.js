2be7b85e0ced8dc6c2e4c233a09c44e7db66c939
3ndrius <andrzejo1991@gmail.com>
2018-04-26 14:58:10 +0200

    Input function

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,12 +3,28 @@ import React, { Component } from 'react';
 
 class Tasks extends Component {
 
+    constructor(props) {
+        super(props);
+        // this.onInputChange = this.onInputChange.bind(this); adding without es6
+        this.state = ({
+            todos: ["Running", "Walking"],
+            currentList: ""
+        });
+    }//end of constructor
 
+    onInputChange = (e) => {
+        this.setState({
+            currentList: e.target.value
+        });
+    }
     render() {
         return(
 
             <div>
-                 Hello tasks
+                <input placeholder="Enter task " value={this.state.currentList} onChange={this.onInputChange}/>
+                <button> Add </button>
+                <br/>
+               {this.state.todos.length === 0 ? " No tasks yet" : "You still have some tasks"}
                 
              </div>
         );//end return
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+// import App from './App';
 import Tasks from './components/Tasks';
 import registerServiceWorker from './registerServiceWorker';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Tasks />, document.getElementById('tasks'));
 
 
